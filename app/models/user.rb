09aa90735b42bdb90ac8356f79e419f3432d9066
jdbs9514 <jdbs9514@gmail.com>
2022-11-07 21:12:08 -0500

    add readme file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :post, foreign_key: 'author_id'
+  has_many :postlas, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
