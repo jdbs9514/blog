622f8858b8304d516989f694460685c5322dda49
jdbs9514 <jdbs9514@gmail.com>
2022-11-15 18:15:37 -0500

    add test to post controller

---
+++ spec/request/posts_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/users/:user_id/posts'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
